561b17962e78b07ab4a8ae0e8958fbc78b985e41
usuario <usuario@DESKTOP-SIFJR7B>
2023-11-10 10:40:37 +0100

    ejercicio4

---
+++ Ejercicio04/src/Ejercicio04.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : Ejercicio04.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+#define MAX 30
+
+int main(void) {
+	int tam;
+	int numeros[MAX];
+
+	do{
+		printf("Introduce el tamaño del array 1-%d: ",MAX);
+		fflush(stdout);
+		scanf("%d",&tam);
+	}while(tam<=0||tam>MAX);
+
+
+	puts("Introduce los datos: ");
+	pedirVector(numeros,tam);
+
+	puts("Los datos introducidos son:");
+	mostrarVector(numeros,tam);
+
+	printf("\n");
+	puts("Los datos movidos una posicion a la izquierda son: ");
+	rotarIzquierda(numeros,tam);
+	mostrarVector(numeros,tam);
+
+
+}
